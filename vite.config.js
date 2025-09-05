516fbe9a0e9a2899143394ae67ad975a140bb679
Meet Vyas <87984759+Meet-Vyas-Dev@users.noreply.github.com>
2025-09-06 00:33:41 +0530

    Update Vite base path for GitHub Pages
    Changed the Vite config 'base' option to '/Parth-G-Bambhaniya.github.io/' to ensure correct asset paths when deploying to GitHub Pages.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Important for GitHub Pages root domain
+  base: '/Parth-G-Bambhaniya.github.io/', // 👈 Add this line
 })
